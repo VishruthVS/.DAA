dac9776a39b27f07e60e829b5ffc19d305fe4938
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-07-31 20:47:13 +0530

    Update 4a.c

--- 4.c
+++ 4.c
@@ -1,54 +1,85 @@
 //String matching
+//Analysis
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-int c=0;
-void strMat(char *t,int n,char *p,int m)
-{
-	for(int i=0;i<n-m;i++)
-	{
-		int j;
-		for(j=0;j<m;j++)
-		{
-			c++;
-			if(t[i+j]!=p[j])
-				break;
-		}
-		if(j==3) break;
-	}
+
+int count = 0;
+
+int stringmatching(char *text, char *pattern, int n, int m) {
+    count = 0;
+    for(int i=0; i<=n-m; i++)
+     {
+        int j=0;
+        while(j<m)
+         {
+            count++;
+            if(pattern[j] != text[i+j]) 
+                break;
+            j++;
+        }
+        if(j==m) {
+            return count;
+        }
+    }
+    
+    return count;
 }
-void writeCount(int n, FILE *fp)
+
+void ploter()
 {
-	fprintf(fp,"%d\t%d\n",n,c);
-	c=0;
+    FILE *f1 =fopen("stringbest.txt", "a");
+    FILE *f2 =fopen("stringworst.txt", "a");
+    FILE *f3 =fopen("stringavg.txt", "a");
+     char *text=(char *)malloc(1000*sizeof(char));
+     char * pattern;
+     for(int i=0;i<1000;i++)
+     *(text+i) = 'a';
+     int m,n;
+      n=1000;
+      m=10;
+      while(m<=1000)
+      {
+        pattern = (char *)malloc(m*sizeof(char));
+        //For Best case
+        for(int i=0; i<m; i++)
+            pattern[i] = 'a';
+            count=0;
+            stringmatching(text, pattern, n,m); 
+            fprintf(f1, "%d\t%d\n", m, count);
+            //printf("%d\t%d\n", m, count);
+            count = 0;
+            //wrost case
+             count=0;
+             pattern[m-1] = 'b';
+             stringmatching(text, pattern, n,m); 
+             fprintf(f2, "%d\t%d\n", m, count);
+              //printf("%d\t%d\n", m, count);
+
+             //For Average Case
+            for(int i=0; i<m; i++)
+            pattern[i] =97+ rand()%3;
+            count=0;
+            stringmatching(text, pattern, n,m); 
+            fprintf(f3,"%d\t%d\n", m, count);
+            //printf("%d\t%d\n", m, count);
+           // for(int i=0; i<m; i++)
+          //  printf("%c ",pattern[i]);
+          //  printf("\n");
+            count=0;
+            free(pattern);
+            if(m<100)
+            m=m+10;
+            else
+            m=m+100;
+      }
+
 }
+
+
+
 void main()
 {
-	char *t,*p;
-	FILE *fB=fopen("SMBest.txt","w");
-	FILE *fA=fopen("SMAvg.txt","w");
-	FILE *fW=fopen("SMWorst.txt","w");
-	srand(time(0));
-	char pB[3]="aaa",pA[3]="aba",pW[3]="aab";
-	for(int n= 10;n<=100;n+=10)
-	{
-		t=(char*)malloc(n*sizeof(char));
-		for(int i=0;i<n;i++)
-			t[i]='a';
-		strMat(t,n,pB,3);
-		writeCount(n,fB);
-		strMat(t,n,pA,3);
-		writeCount(n,fA);
-		strMat(t,n,pW,3);
-		writeCount(n,fW);		
-	}
-	fclose(fB);
-	fclose(fW);
-	fclose(fA);
-	FILE *fg = fopen("SMplot.gnu","w");
-fprintf(fg,"set yrange[0:*]\nset xlabel \"n\"\nset ylabel \"t\"\nplot \"SMBest.txt\" w l ti \"Best Case\",\"SMAvg.txt\" w l ti \"Average Case\", \"SMWorst.txt\" w l ti \"Worst Case\"\nset term png\nset output \"SMG.png\"\nreplot\nset term x11");
-fclose(fg);
-system("gnuplot \"SMplot.gnu\"");
-system("eog SMG.png");
+  ploter();
 }
