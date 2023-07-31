44cfc98ac9ba33ca1425bee30c620a1df8a76766
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-08-01 00:25:32 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,104 +1,72 @@
+/*TESTER IS USED TO CHECK THE CORRECTNESS OF THE ALGORITH*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
-int c=0;
-
-void swap(int *a,int *b)
+int count;
+void merge(int *arr,int beg,int mid,int end)
 {
-	int t=*a;
-	*a=*b;
-	*b=t;
-}
+   int i,j,k;
+   int n1=(mid-beg)+1;
+   int n2=end-mid;
+   int left[n1],right[n2];
+   for(i=0;i<n1;i++)
+   left[i]=arr[beg+i];
+   for(j=0;j<n2;j++)
+   right[j]=arr[mid+j+1];
+    i=0;j=0;k=beg;
+   while(i<n1&&j<n2)
+   {
+        count++;
+       if(left[i]<=right[j])
+        arr[k]=left[i++];
+       else
+        arr[k]=right[j++];
+       k++;
+   }
 
-void heapify(int *a,int n, int ro)
-{
-	int lar=ro,l=2*ro,r=2*ro+1;
-	c+=1;
-	if(l<=n&&a[l]>a[lar])
-		lar=l;
-	if(r<=n&&a[r]>a[lar])
-		lar=r;
-	if(lar!=ro)
-	{
-		swap(&a[lar],&a[ro]);
-		heapify(a,n,lar);
-	}
-}
+    while(i<n1)
+    arr[k++]=left[i++];
+     while(j<n2)
+    arr[k++]=right[j++];
 
-void heapSort(int *a,int n)
-{
-	for(int i=n/2;i>=1;i--)
-		heapify(a,n,i);
-	for(int i=n;i>1;i--)
-	{
-		swap(&a[1],&a[i]);
-		heapify(a,i-1,1);
-	}
 }
 
-void writeCount(int n,FILE *fp)
+
+
+void mergesort(int *arr,int beg,int end)
 {
-	fprintf(fp,"%d\t%d\n",n,c);
-	c=0;
+    if(beg<end)
+    {
+     int mid=(beg+end)/2;
+     mergesort(arr,beg,mid);
+     mergesort(arr,mid+1,end);
+     merge(arr,beg,mid,end);
+    }
 }
-void writeArr(int *a,int n,FILE *fp)
-{	
-	fprintf(fp,"Array : \n");
-	for(int i = 0;i<n;i++)
-		fprintf(fp,"%d\t",a[i]);
-	fprintf(fp,"\n");
-}
-void recordCount(int *a,int n,FILE *f,FILE *fp)
-{
-	writeArr(a,n,f);
-	heapSort(a,n);
-	writeCount(n,fp);
-}
+
+
+
 void main()
 {
-	FILE *f=fopen("HSArrayInfo.txt","w");
-	FILE *fc=fopen("HSBest.txt","w");
-	int *a;
-	srand(time(0));
-	fprintf(f,"\t\tBest Case \n");
-	for(int n=10;n<=100;n+=10)
-	{
-		a=(int *)malloc((n+1)*sizeof(int));
-		a[1]=rand()%100;
-		for(int i=2;i<=n;i++)
-			a[i]=a[i-1];
-		recordCount(a,n,f,fc);
-		free(a);
-	}
-	fclose(fc);
-	fc=fopen("HSAvg.txt","w");
-	fprintf(f,"\t\tAverage Case \n");
-	for(int n=10;n<=100;n+=10)
-	{
-		a=(int *)malloc((n+1)*sizeof(int));
-		for(int i=1;i<=n;i++)
-			a[i]=rand()%10;
-		recordCount(a,n,f,fc);
-		free(a);
-	}
-	fclose(fc);
-	fc=fopen("HSWorst.txt","w");
-	fprintf(f,"\t\tWorst Case \n");
-	for(int n=10;n<=100;n+=10)
-	{
-		a=(int *)malloc((n+1)*sizeof(int));
-		a[1]=rand()%100;
-		for(int i=2;i<=n;i++)
-			a[i]=a[i-1]+rand()%10;
-		recordCount(a,n,f,fc);
-		free(a);
-	}
-	fclose(fc);
-	fclose(f);
-	FILE *fg = fopen("HSplot.gnu","w");
-	fprintf(fg,"set xlabel \"n\"\nset ylabel \"t\"\nplot \"HSBest.txt\" w l ti \"Best Case\", \"HSWorst.txt\" w l smooth bezier ti \"Worst Case\", \"HSAvg.txt\" w l ti \"Average Case\"\nset term png\nset output \"HSG.png\"\nreplot\nset term x11");
-	fclose(fg);
-	system("gnuplot \"HSplot.gnu\"");
-	system("eog HSG.png");	
+  int *arr, n;
+   printf("ENTER THE NUMBER OF ELEMENTS\n");
+   scanf("%d",&n);
+
+   arr=(int *)malloc(sizeof(int)*n);
+   printf("ENTER THE ELEMENTS OF THE ARRAY\n");
+      for(int i=0;i<n;i++)
+       scanf("%d",&arr[i]);
+
+  printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n"); 
+    for(int i=0;i<n;i++)
+       printf("%d ",arr[i]);
+    printf("\n");
+
+      mergesort(arr,0,n-1);
+
+      printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n"); 
+    for(int i=0;i<n;i++)
+       printf("%d ",arr[i]);
+       printf("\n");
+       printf("\n");
 }
